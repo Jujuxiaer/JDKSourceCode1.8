cf0789e5680b89cfde597a289947afc0ca825e3b
juzhh <juzhh@esensoft.com>
2022-10-14 18:02:08 +0800

    解决编译报错，jdk版本用的是jdk1.8.0_241

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
+
 /**
  * @author wupx
  * @date 2019/10/25
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a", "1");
     }
 }
